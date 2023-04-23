e197c95e35437e812f92f4782b53fcebd501974e
RobertaReis <roberta_reis@uol.com.br>
2023-04-23 00:54:32 +0000

    Arquivos IaC v.1

---
+++ iac1b.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+echo "Criando usuários SEM SENHAS......"
+
+useradd carlos -m -s /bin/bash -G GRP_ADM
+useradd maria -m -s /bin/bash -G GRP_ADM
+useradd joao -m -s /bin/bash -G GRP_ADM
+
+useradd debora -m -s /bin/bash -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -G GRP_VEN
+useradd roberto -m -s /bin/bash -G GRP_VEN
+
+useradd josefina -m -s /bin/bash -G GRP_SEC
+useradd amanda -m -s /bin/bash -G GRP_SEC
+useradd rogerio -m -s /bin/bash -G GRP_SEC
+
+echo "Finalizando criação de usuários SEM SENHA......"
